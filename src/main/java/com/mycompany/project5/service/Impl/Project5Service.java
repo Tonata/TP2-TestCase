6d1593a0e965abd018078ccfb8897a1d9c2c323e
student <student@student-virtual-machine>
2014-02-22 20:50:10 +0200

    Merge origin/master
    Conflicts:
    	pom.xml

---
+++ src/main/java/com/mycompany/project5/service/Impl/Project5Service.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.project5.service.Impl;
+
+import com.mycompany.project5.service.Numbers;
+
+/**
+ *
+ * @author student
+ */
+public class Project5Service implements Numbers{
+    public int [] values(){
+        int [] a = {4,90,2,5,67};
+        return a;
+    }
+}
